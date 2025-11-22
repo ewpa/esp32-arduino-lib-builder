a06e2e934fc71aedbd2393b8a1464b598d1b20ab
Ewan Parker <34030942+ewpa@users.noreply.github.com>
2025-11-22 08:38:14 +0000

    Default to build against 'ewpa' repositories.

--- tools/config.sh
+++ tools/config.sh
@@ -6,7 +6,7 @@ if [ -z $IDF_PATH ]; then
 fi
 
 if [ -z $IDF_BRANCH ]; then
-    IDF_BRANCH="release/v5.1"
+    IDF_BRANCH="release/v5.1+ewpa"
 fi
 
 if [ -z $AR_PR_TARGET_BRANCH ]; then
@@ -25,7 +25,7 @@ if [ -z $IDF_TARGET ]; then
 fi
 
 # Owner of the target ESP32 Arduino repository
-AR_USER="${GITHUB_REPOSITORY_OWNER:-espressif}"
+AR_USER="${GITHUB_REPOSITORY_OWNER:-ewpa}"
 
 # The full name of the repository
 AR_REPO="$AR_USER/arduino-esp32"
