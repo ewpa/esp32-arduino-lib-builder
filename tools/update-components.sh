0f50b54702cc2117a3472e1a320ced5153127173
Ewan Parker <34030942+ewpa@users.noreply.github.com>
2025-11-22 20:22:12 +0000

    Check out contemporary version of TinyUSB component.

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -10,8 +10,9 @@ TINYUSB_REPO_URL="https://github.com/hathach/tinyusb.git"
 TINYUSB_REPO_DIR="$AR_COMPS/arduino_tinyusb/tinyusb"
 if [ ! -d "$TINYUSB_REPO_DIR" ]; then
     git clone "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"
-else
-    git -C "$TINYUSB_REPO_DIR" fetch && \
-    git -C "$TINYUSB_REPO_DIR" pull --ff-only
 fi
 if [ $? -ne 0 ]; then exit 1; fi
+
+cd "$TINYUSB_REPO_DIR"
+git checkout 9ca4bc89a7e285796e8102eb7a8fd1bbc6fb9938
+cd -
